4778c5bde00af1f78c47d0b1843681a56f985797
V587MrZ <u6972739@anu.edu.au>
2023-04-02 16:02:47 +1000

    add meta

---
+++ build/meta_pkg/colcon_command_prefix_build.sh
@@ -0,0 +1,6 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/levi/tut_interface/install/base_interfaces_demo/share/base_interfaces_demo/package.sh"
+. "/home/levi/tut_interface/install/cpp04_param/share/cpp04_param/package.sh"
+. "/home/levi/tut_interface/install/cpp01_topic/share/cpp01_topic/package.sh"
+. "/home/levi/tut_interface/install/cpp02_srv/share/cpp02_srv/package.sh"
+. "/home/levi/tut_interface/install/cpp03_action/share/cpp03_action/package.sh"
